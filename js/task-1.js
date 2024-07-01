6d4ff9a575906d5f8c03d0286a67f4e0c921ddf6
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-07-01 16:06:57 +0300

    asdasd

--- js/task-1.js
+++ js/task-1.js
@@ -1,44 +1,11 @@
-const getUserNames = (users) => {
-  const userName = users.map((user) => user.name);
-  return userName;
-};
+const categories = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categories.length}`);
 
-console.log(
-  getUserNames([
-    {
-      name: "Moore Hensley",
-      email: "moorehensley@indexia.com",
-      balance: 2811,
-    },
-    {
-      name: "Sharlene Bush",
-      email: "sharlenebush@tubesys.com",
-      balance: 3821,
-    },
-    {
-      name: "Ross Vazquez",
-      email: "rossvazquez@xinware.com",
-      balance: 3793,
-    },
-    {
-      name: "Elma Head",
-      email: "elmahead@omatom.com",
-      balance: 2278,
-    },
-    {
-      name: "Carey Barr",
-      email: "careybarr@nurali.com",
-      balance: 3951,
-    },
-    {
-      name: "Blackburn Dotson",
-      email: "blackburndotson@furnigeer.com",
-      balance: 1498,
-    },
-    {
-      name: "Sheree Anthony",
-      email: "shereeanthony@kog.com",
-      balance: 2764,
-    },
-  ])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+categories.forEach((category) => {
+  // console.log(category);
+  const h2 = category.querySelector("h2");
+  console.log("Category:", h2.textContent);
+  const ul = category.querySelectorAll("li");
+  // console.log(ul);
+  console.log("Elements:", ul.length);
+});
--- js/task-2.js
+++ js/task-2.js
@@ -1,62 +1,37 @@
-const getUsersWithFriend = (users, friendName) => {
-  const userWithFriend = users.filter((user) =>
-    user.friends.includes(friendName)
-  );
-  return userWithFriend;
-};
-const allUsers = [
+const images = [
   {
-    name: "Moore Hensley",
-    friends: ["Sharron Pace"],
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    name: "Sharlene Bush",
-    friends: ["Briana Decker", "Sharron Pace"],
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    name: "Ross Vazquez",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
   {
-    name: "Elma Head",
-    friends: ["Goldie Gentry", "Aisha Tran"],
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
   },
   {
-    name: "Carey Barr",
-    friends: ["Jordan Sampson", "Eddie Strong"],
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
   },
   {
-    name: "Blackburn Dotson",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-  },
-  {
-    name: "Sheree Anthony",
-    friends: ["Goldie Gentry", "Briana Decker"],
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
   },
 ];
 
-console.log(getUsersWithFriend(allUsers, "Briana Decker"));
-// [
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+const gallery = document.querySelector(".gallery");
 
-console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+const galleryItems = images
+  .map(({ url, alt }) => {
+    return `<li><img src="${url}", alt="${alt}"
+  width="300"</li>`;
+  })
+  .join("");
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross")); // []
+gallery.insertAdjacentHTML("beforeend", galleryItems);
