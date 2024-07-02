f8d96513ee671e68d032aa9c4f5efb64a0bffcca
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-07-02 21:14:40 +0300

    asdasd

--- js/task-1.js
+++ js/task-1.js
@@ -2,10 +2,8 @@ const categories = document.querySelectorAll("#categories .item");
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((category) => {
-  // console.log(category);
   const h2 = category.querySelector("h2");
   console.log("Category:", h2.textContent);
   const ul = category.querySelectorAll("li");
-  // console.log(ul);
   console.log("Elements:", ul.length);
 });
