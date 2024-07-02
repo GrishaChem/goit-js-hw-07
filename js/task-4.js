82a85824f27c4127f630183e236b039edcf57d08
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-07-02 10:47:22 +0300

    asdasd

---
+++ js/task-4.js
@@ -0,0 +1,16 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+
+  const info = {
+    email: elements.email.value,
+    password: elements.email.value,
+  };
+  console.log(info);
+  event.currentTarget.reset();
+}
